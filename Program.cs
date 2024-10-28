af54b74fac26eab980d0b5a318cf432d699700a6
falau <falau@LENOVO>
2024-10-28 12:02:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Set1Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar: ");
+            int numar = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti valoarea lui k (cifra de la sfarsit pe care o doriti): ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (k <= 0)
+            {
+                Console.WriteLine("Valoarea lui k trebuie sa fie mai mare decat 0");
+            }
+            else
+            {
+                int cifraDorita = (numar / (int)Math.Pow(10, k-1)) % 10;
+                Console.WriteLine($"A {k}-a cifra de la sfarsitul numarului este {cifraDorita}");
+            } 
+            
+
+
+        }
+    }
+}
